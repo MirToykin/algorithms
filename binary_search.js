f0ba5543f05386e317b39415ad773af50c0a11b4
Miroslav <miroslavtoykin@gmail.com>
2022-07-11 19:47:01 +0300

    binary search 1

--- binary_search.js
+++ binary_search.js
@@ -2,14 +2,12 @@ const prompt = require('prompt');
 const list = [1, 3, 5, 7, 8, 10, 12, 13];
 
 const binarySearch = (item) => {
-    item = +item;
 
     let min = 0;
     let max = list.length - 1;
 
     while (min <= max) {
-        let mid = min === max ? min : Math.floor((max - min)/2);
-        console.log(item, min, max, mid);
+        let mid = Math.floor((max + min)/2);
 
         if (list[mid] === item) {
             return mid;
